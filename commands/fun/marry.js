5706671606906c733fa5073f02099397723718a0
Glowstonedevs <88795355+Glowstonedevs@users.noreply.github.com>
2021-08-25 07:00:36 +0400

    lol

--- commands/fun/marry.js
+++ commands/fun/marry.js
@@ -26,19 +26,44 @@ module.exports = {
 
         role = message.guild.roles.cache.find(r => r.name.toLowerCase() === 'married')
 
-        if (role) {
-            message.member.roles.add(role)
-            member.roles.add(role)
-        }
+        const filter1 = (m) => m.author.id === message.author.id
+        const filter2 = (m) => m.author.id === member.id
 
-        let embed  = new Discord.MessageEmbed()
-        embed.setColor(options.color)
-        .setTitle(`🎊 Marriage 👯‍♂️`)
-        .setDescription(`<@${message.author.id}> and <@${member.id}>\n have married\n\n*Note: Consent will be added in the next update, so for now L dance*`)
-        .setImage("https://static.reuters.com/resources/r/?m=02&d=20190524&t=2&i=1390410346&r=LYNXNPEF4N0CH&w=640")
-        .setTimestamp()
-        .setFooter(` | ${message.guild.name}`, message.guild.iconURL({ dynamic: true }));
+        message.channel.send(`Do you promise to love, honor, cherish, and protect him/her, forsaking all others, and holding only onto him/her forevermore <@${message.author.id}>?\n\`Yes/No\``)
+        message.channel
+        .awaitMessages(filter1, {max:1, time: 60000, errors: ['time']})
+        .then((collected)=>{
+            const msg = collected.first()
 
-        message.channel.send(embed)
+            if (msg.content.toLowerCase() == 'yes'){
+                message.channel.send(`And <@${member.id}>, do you take <@${message.author.id}> to be your husband/wife??\n\`Yes/No\``)
+                message.channel
+                .awaitMessages(filter2, {max:1, time: 60000, errors: ['time']})
+                .then((collected)=> {
+                    const msg = collected.first()
+                    if (msg.content.toLowerCase() == 'yes'){
+                        if (role) {
+                            message.member.roles.add(role)
+                            member.roles.add(role)
+                        }
+                        let embed  = new Discord.MessageEmbed()
+                        embed.setColor(options.color)
+                        .setTitle(`🎊 Marriage 👯‍♂️`)
+                        .setDescription(`<@${message.author.id}> and <@${member.id}>\n have married\n`)
+                        .setImage("https://static.reuters.com/resources/r/?m=02&d=20190524&t=2&i=1390410346&r=LYNXNPEF4N0CH&w=640")
+                        .setTimestamp()
+                        .setFooter(` | ${message.guild.name}`, message.guild.iconURL({ dynamic: true }));
+
+                        message.channel.send("**You may now kiss the bride**")
+                        return message.channel.send(embed)
+                    }else{
+                        return message.channel.send(`Yikes! That's rough buddy <@${message.author.id}>`)
+                    }
+                })
+            }else{
+                return message.channel.send("Sheesh, imagine proposing and rejecting it yourself, you attention seeking whore!")
+            }
+        })
+
     }
 }
